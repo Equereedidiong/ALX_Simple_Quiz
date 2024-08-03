66c80aea3e8a903616113b602ef20823a7964db3
A'DI <EquereAedidiong@gmail.com>
2024-08-03 07:13:06 +0100

    0. Implement a Simple Interactive Quiz

---
+++ quiz.js
@@ -0,0 +1,65 @@
+
+// let checkAnswer = function() {
+
+//     let correctAnswer = 4";
+    
+//     const userAnswer = document.querySelector(`input[name="quiz"]:checked');
+
+// }
+
+
+
+// // const userAnswer = document.querySelector(`input[ name ="quiz"]:checked');
+
+
+// // const Feedback = document.getElementById('feedback');
+
+// // if (userAnswer) {
+// //     if ( correctAnswer === userAnswer.value) {
+// //           Feedback.textContent = "Your Answer is Correct";
+// //     } else {
+
+// //           Feedback.textContent = "That's Incorrect, Try again!";
+
+// //     } 
+
+// // }
+
+
+
+let checkAnswer = function () {
+    
+    correctAnswer = "4";
+
+    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+
+    const Feedback = document.getElementById('feedback');
+
+    if (userAnswer) {
+       
+        if ( correctAnswer === userAnswer.value) {
+          Feedback.textContent = "Your Answer is Correct";
+        } else  {
+
+          Feedback.textContent = "That's Incorrect, Try again!";
+    
+        }
+    } 
+        
+    // else {
+
+    //         Feedback.textContent = " Please enter an answer";
+    //     }
+}
+
+let Btn = document.getElementById ('submit-answer');
+    
+    Btn.addEventListener ('click', function (submit){
+
+        console.log(submit.target)
+        console.log(submit);
+
+        
+    })
+
+ 
